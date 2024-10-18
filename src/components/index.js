173e5900c45a7e5d88d881b20b4b25e462c3fb39
sakshi patial <sakshipatial154@gmail.com>
2024-10-18 17:29:36 +0530

    currency convertor using react and custom hook

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import InputBox from './InputBox'
+
+export {InputBox}
+
+// for  bigger projects  where lots of  componets are there to export , In  that case we use this approch  for  exporting  components.
